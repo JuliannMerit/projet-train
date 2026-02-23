6a3e52a541d7272138f73e7f50a008adeb94e1ee
JuliannM <juliann.merit@gmail.com>
2026-02-23 15:28:31 +0100

    Init project

---
+++ src/main/java/com/example/projettrain/ProjetTrainApplication.java
@@ -0,0 +1,13 @@
+package com.example.projettrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetTrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetTrainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projettrain/ProjetTrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projettrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetTrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

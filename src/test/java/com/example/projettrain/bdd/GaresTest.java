e1b332f89119ffd32812b29cef8e8afb80d446f5
felix_ltrtr <felix.letertre@gmail.com>
2026-03-25 09:37:05 +0100

    modifie nom classe de test

--- src/test/java/com/example/projettrain/bdd/GaresTest.java
+++ src/test/java/com/example/projettrain/bdd/GaresTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-public class DatabaseTest {
+public class GaresTest {
 
     @Test
     public void creeDesInstancesDansLaTableGares(){
